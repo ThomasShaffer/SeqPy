b67dbf27882e19933ad067da460040406dd7467d
Your Name <you@example.com>
2021-05-06 00:59:00 -0700

    updated with exception raises

--- tests/test_complement.py
+++ tests/test_complement.py
@@ -6,7 +6,7 @@ class test_correction(unittest.TestCase):
 
 	def test_empty_string(self):
 		empty_sequence = dna('')
-		self.assertEqual(empty_sequence.complement(), '')
+		self.assertRaises(Exception)
 	
 	def test_lower_case(self):
 		lower_case = dna('agtgtcgatg')
@@ -14,7 +14,7 @@ class test_correction(unittest.TestCase):
 
 	def test_non_dna(self):
 		non_dna = dna('AGT!Q')
-		self.assertFalse(non_dna.sequence[1] == non_dna.complement()[1])
+		self.assertRaises(Exception)
 
 	def test_normal_string(self):
 		normal_dna = dna('AGTCAGTAGAC')
