b18db703652c2c4a8dd16c20bf83094f9ac038da
Thomas Shaffer <tshaffer@berkeley.edu>
2021-06-28 23:50:35 -0700

    add test cases for fastq parsing

--- tests/test_fastq_parser.py
+++ tests/test_fastq_parser.py
@@ -5,13 +5,39 @@ import unittest
 class test_correction(unittest.TestCase):
 
     def test_correct_fastq(self):
-        fastq = FileParser('fastq.fq')
-        self.assertTrue(fastq.sequences.read_counts == 2)
+    
+        """ fastq.fq
+        
+            @FASTQ_Read1
+            AGTGATGAGCACACGTGAC
+            +Random
+            IPQLRQRLQEPRLRQERQL
+            @FASTQ_Read2
+            TAUTAUTACGUTUGAUGUAGUAGUTUAGUCU
+            +RandomAgain
+            1#12310Lofalsdfllliifpdaifdpfio
 
 
+        """
+
+        fastq = FileParser('fastq.fq').sequences
+        self.assertTrue(fastq.read_counts == 2)
+        self.assertEqual(fastq.reads['FASTQ_Read1'][0], 'AGTGATGAGCACACGTGAC')
+        self.assertEqual(fastq.reads['FASTQ_Read2'][1], '1#12310Lofalsdfllliifpdaifdpfio')
 
 
+    def test_nonmatching_sizes(self):
+        
+        """ mismatchfastq.fastq
 
+            @FASTQ_Read1
+            AGTGATGACACGTGACGT
+            +FASTQ_Read1
+            ifadfiii--!__!#!!@@#$wqwe!!
+        
+        """
+
+        self.assertRaises(AssertionError, FileParser, 'mismatchfastq.fastq')
 
 
 
